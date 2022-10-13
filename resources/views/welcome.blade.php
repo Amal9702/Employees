95ae8c7750f46dee301739d8c0ccbccf95064102
Amal Alshomrani <amal.alshomrani.cs@gmail.com>
2022-10-13 16:14:12 +0300

    add task 3 to home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -67,6 +67,22 @@
                                     </ul>
                                 </div>
                             </div>
+                            <div class="border-t border-gray-200 dark:border-gray-700  md:border-t py-4 mt-4">
+                                <div class="flex items-center">
+                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path></svg>
+                                    <div class="ml-4 text-lg leading-7 font-semibold">Task 3</div>
+                                </div>
+
+                                <div class="ml-12">
+                                    <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm flex">
+                                        <svg fill="none" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="-mt-px w-5 h-5 text-gray-400"><path d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+
+                                        <a href="https://github.com/Amal9702/Employees" class="ml-1 underline">
+                                            Github
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
 
                     </div>
